dce09a462e36a751de00692aab5c56205616e8e1
mike castleman <m@mlcastle.net>
2025-12-03 03:31:37 -0500

    cleaner?

--- day03.ts
+++ day03.ts
@@ -1,26 +1,24 @@
-import { range, readSplit, show, sumBy } from './util.ts';
+import { readSplit, show, sumBy } from './util.ts';
+
+declare global {
+  interface Array<T> {
+    max(): number;
+  }
+}
+
+Array.prototype.max = function max() {
+  return this.reduce((acc, x) => acc > x ? acc : x, -Infinity);
+};
 
 const parse = (x: string) => Array.from(x).map((ch) => Number(ch));
 
 const input = (await readSplit(3, '\n', false)).map(parse);
 
-const joltage = (bank: readonly number[]): number => {
-  const d1 = Math.max(...bank.toSpliced(bank.length - 1));
-  const i1 = bank.indexOf(d1);
-  if (i1 < 0) {
-    throw new Error();
-  }
-  const d2 = Math.max(...bank.toSpliced(0, i1 + 1));
-  return d1 * 10 + d2;
-};
-
-await show(sumBy(input, joltage));
-
-const highJoltage = (bank: readonly number[]) => {
+const joltage = (n: number) => (bank: readonly number[]) => {
   let result = 0;
   let miniBank = bank;
-  for (let i = 11; i >= 0; --i) {
-    const d = Math.max(...miniBank.toSpliced(miniBank.length - i));
+  for (let i = n - 1; i >= 0; --i) {
+    const d = miniBank.toSpliced(miniBank.length - i).max();
     const idx = miniBank.indexOf(d);
     if (idx < 0) {
       throw new Error(`${d} ${miniBank}`);
@@ -31,4 +29,6 @@ const highJoltage = (bank: readonly number[]) => {
   return result;
 };
 
-await show(sumBy(input, highJoltage));
+await show(sumBy(input, joltage(2)));
+
+await show(sumBy(input, joltage(12)));
