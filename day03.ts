5e82d242304231056bf3d14f4c187ac8b43078ba
mike castleman <m@mlcastle.net>
2025-12-03 19:31:27 -0500

    refactoring? what even is wrong with me

--- day03.ts
+++ day03.ts
@@ -1,15 +1,5 @@
 import { readSplit, show, sumBy } from './util.ts';
 
-declare global {
-  interface Array<T> {
-    max(): number;
-  }
-}
-
-Array.prototype.max = function max() {
-  return this.reduce((acc, x) => acc > x ? acc : x, -Infinity);
-};
-
 const parse = (x: string) => Array.from(x).map((ch) => Number(ch));
 
 const input = (await readSplit(3, '\n', false)).map(parse);
@@ -18,9 +8,15 @@ const joltage = (n: number) => (bank: readonly number[]) => {
   let result = 0;
   let miniBank = bank;
   for (let i = n - 1; i >= 0; --i) {
-    const d = miniBank.toSpliced(miniBank.length - i).max();
-    const idx = miniBank.indexOf(d);
-    if (idx < 0) {
+    let d = -Infinity;
+    let idx: number | undefined;
+    for (let j = 0; j < miniBank.length - i; j++) {
+      if (miniBank[j] > d) {
+        d = miniBank[j];
+        idx = j;
+      }
+    }
+    if (typeof idx !== 'number') {
       throw new Error(`${d} ${miniBank}`);
     }
     result = 10 * result + d;
