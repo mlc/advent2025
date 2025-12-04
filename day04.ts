1c6f549ebe543c508247ca8f9c662a9936e224e0
mike castleman <m@mlcastle.net>
2025-12-04 00:11:00 -0500

    day 04

---
+++ day04.ts
@@ -0,0 +1,29 @@
+import { map2D, neighbors, readSplit, show, sumBy } from './util.ts';
+
+const parse = (x: string) => Array.from(x);
+
+const input = (await readSplit(4, '\n', false)).map(parse);
+
+const removables = (arr: string[][]) =>
+  map2D(
+    arr,
+    (pos, c) =>
+      c === '@' &&
+      sumBy(neighbors(pos, true), ([x, y]) => arr[x]?.[y] === '@') < 4,
+  );
+
+await show(sumBy(removables(input).map((r) => sumBy(r))));
+
+let total = 0;
+let mutated = input;
+while (true) {
+  const rm = removables(mutated);
+  const thisR = sumBy(rm.map((r) => sumBy(r)));
+  total += thisR;
+  if (thisR === 0) {
+    break;
+  }
+  mutated = map2D(mutated, ([x, y], orig) => rm[x][y] ? '.' : orig);
+}
+
+await show(total);
